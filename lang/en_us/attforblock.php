ce208a136d0b3eed7943dd112a1b3496e9d04309
Artem Andreev <andreev.artem@gmail.com>
2011-08-01 22:10:42 +0400

    Fixed en_us language strings

--- lang/en_us/attforblock.php
+++ lang/en_us/attforblock.php
@@ -2,9 +2,9 @@
       // block_attendance.php - created with Moodle 1.5.3+ (2005060230)
 
 
-$string['strftimedm'] = '%%m.%%d';
-$string['strftimedmy'] = '%%m.%%d.%%Y';
-$string['strftimedmyw'] = '%%m.%%d.%%y&nbsp;(%%a)';
-$string['strftimeshortdate'] = '%%m.%%d.%%Y';
+$string['strftimedm'] = '%m.%d';
+$string['strftimedmy'] = '%m.%d.%Y';
+$string['strftimedmyw'] = '%m.%d.%y&nbsp;(%a)';
+$string['strftimeshortdate'] = '%m.%d.%Y';
 
 ?>
