5210e5c79f17923f314e4f6c52397c44f0c7e004
Artem Andreev <andreev.artem@gmail.com>
2011-12-02 14:03:13 +0400

    Fixed renaming's regression

--- renderhelpers.php
+++ renderhelpers.php
@@ -214,12 +214,12 @@ function construct_full_user_stat_html_table($attforblock, $course, $user) {
 
         $gradeable = $attforblock->grade > 0;
         $statuses = att_get_statuses($attforblock->id);
-        $userstatusesstat = get_user_statuses_stat($attforblock->id, $course->startdate, $user->id);
-        $stat['completed'] = get_user_taken_sessions_count($attforblock->id, $course->startdate, $user->id);
+        $userstatusesstat = att_get_user_statuses_stat($attforblock->id, $course->startdate, $user->id);
+        $stat['completed'] = att_get_user_taken_sessions_count($attforblock->id, $course->startdate, $user->id);
         $stat['statuses'] = $userstatusesstat;
         if ($gradeable) {
             $grade = att_get_user_grade($userstatusesstat, $statuses);
-            $maxgrade = att_get_user_max_grade(get_user_taken_sessions_count($attforblock->id, $course->startdate, $user->id), $statuses);
+            $maxgrade = att_get_user_max_grade(att_get_user_taken_sessions_count($attforblock->id, $course->startdate, $user->id), $statuses);
             if (!$decimalpoints = grade_get_setting($course->id, 'decimalpoints')) {
                 $decimalpoints = $CFG->grade_decimalpoints;
             }
