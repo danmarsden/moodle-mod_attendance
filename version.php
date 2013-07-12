fe773fcdc6d820a5f78d0cb74d738a63b2d39a61
Dan Marsden <dan@danmarsden.com>
2013-07-12 11:04:44 +1000

    slightly better check - there's probably a cfg var I could check here instead of using try/catch

--- version.php
+++ version.php
@@ -33,13 +33,16 @@ $module->component = 'mod_attendance';
 // TODO: remove this asap.
 if (defined('MOODLE_INTERNAL')) { // Only run if config.php has already been included.
     global $DB;
+    $moduleexists = false;
+
     try {
-        if ($DB->record_exists('modules', array('name' =>'attforblock'))) {
-            require_once('locallib.php');
-            attforblock_upgrade();
-        }
+        $moduleexists = $DB->record_exists('modules', array('name' =>'attforblock'));
     } catch (Exception $e) {
         // Probably a fresh install - modules table doesn't exist
+    }
+    if ($moduleexists) {
+        require_once('locallib.php');
+        attforblock_upgrade();
     }
 
 }
