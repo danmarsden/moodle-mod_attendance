feca3fd8a7e4ad9a3ccdaf5520eef46793592bbe
NeillM <neill.magill@nottingham.ac.uk>
2013-09-02 09:36:53 +1200

    Security fix to prevent access to other students attendance

--- view.php
+++ view.php
@@ -63,7 +63,14 @@ $PAGE->navbar->add(get_string('attendancereport', 'attendance'));
 
 $output = $PAGE->get_renderer('mod_attendance');
 
-$userid = isset($pageparams->studentid) ? $pageparams->studentid : $USER->id;
+if (isset($pageparams->studentid) && $USER->id != $pageparams->studentid) {
+    // Only users with proper permissions should be able to see any user's individual report.
+    require_capability('mod/attendance:viewreports', $PAGE->context);
+    $userid = $pageparams->studentid;
+} else {
+    // A valid request to see another users report has not been sent, show the user's own.
+    $userid = $USER->id;
+}
 $userdata = new attendance_user_data($att, $userid);
 
 echo $output->header();
