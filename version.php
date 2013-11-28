2a191a65c033eda4ecc93b24ff778b6ae382dc28
Tim Gus <tim.gusak@remote-learner.net>
2013-11-28 16:27:20 -0500

    CONTRIB-4754 Added upgrade step to account for name change from "attforblock" to "attendance": - updated itemmodule field in the grade tables - updated role_capabilities table to preserve old capabilities - deleted old data from capabilities and role_capabilities tables

--- version.php
+++ version.php
@@ -22,7 +22,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$module->version  = 2013070404;
+$module->version  = 2013070405;
 $module->requires = 2012062504;
 $module->release = '2.4.3';
 $module->maturity  = MATURITY_STABLE;
