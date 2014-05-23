5909afa1a459b664e28e85402641f9a3a4ae1478
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2014-05-23 11:21:10 +0100

    changes attforblock to attendance

--- locallib.php
+++ locallib.php
@@ -1259,7 +1259,7 @@ class attendance {
         list($sql, $params) = $DB->get_in_or_equal($sessionsids);
         $DB->delete_records_select('attendance_log', "sessionid $sql", $params);
         $DB->delete_records_list('attendance_sessions', 'id', $sessionsids);
-        $this->log('sessions deleted', null, get_string('sessionsids', 'attforblock').implode(', ', $sessionsids));
+        $this->log('sessions deleted', null, get_string('sessionsids', 'attendance').implode(', ', $sessionsids));
     }
 
     public function update_sessions_duration($sessionsids, $duration) {
@@ -1273,7 +1273,7 @@ class attendance {
             $DB->update_record('attendance_sessions', $sess);
         }
         
-        $this->log('sessions duration updated', $this->url_manage(), get_string('sessionsids', 'attforblock').implode(', ', $sessionsids));
+        $this->log('sessions duration updated', $this->url_manage(), get_string('sessionsids', 'attendance').implode(', ', $sessionsids));
     }
 
     public function remove_status($statusid) {
