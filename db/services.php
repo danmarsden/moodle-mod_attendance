30518587692960b303e2d17782463afa82307bf9
Michael Hughes <michaelhughes@strath.ac.uk>
2014-06-03 11:37:45 +0100

    Added import of web service implementation from Strathclyde version of modattforblock

---
+++ db/services.php
@@ -0,0 +1,11 @@
+<?php
+$functions = array(
+	'mod_attforblock_get_attendance_data'=> array(
+		'classname'=>'attforblock_external',
+		'methodname'=>'get_attendance_data',
+		'classpath'	=>'mod/attforblock/externallib.php',
+		'description'=>'Get attendance data for attendance activity',
+		'type'=>'read',
+		'testclientpath' => 'mod/attforblock/ws_test.php'
+	)
+);
