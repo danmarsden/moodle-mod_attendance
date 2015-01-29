0de00a526524ef7079172dcd15f1ab82cd798604
Antonio Carlos Mariani <a.c.mariani@inf.ufsc.br>
2015-01-28 22:38:14 -0200

    Translation to portuguese (pt_br)

---
+++ lang/pt_br/attendance.php
@@ -0,0 +1,241 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'attendance', language 'pt_br'
+ *
+ * @package   mod_attendance
+ * @copyright  2011 Artem Andreev <andreev.artem@gmail.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['attendance:addinstance'] = 'Adicionar uma nova atividade de presença';
+$string['Aacronym'] = 'Au';
+$string['Afull'] = 'Ausente';
+$string['Eacronym'] = 'Di';
+$string['Efull'] = 'Dispensado';
+$string['Lacronym'] = 'At';
+$string['Lfull'] = 'Atrasado';
+$string['Pacronym'] = 'Pr';
+$string['Pfull'] = 'Presente';
+$string['acronym'] = 'Acrônimo';
+$string['add'] = 'Adicionar';
+$string['addmultiplesessions'] = 'Adicionar várias sessões';
+$string['addsession'] = 'Adicionar sessão';
+$string['allcourses'] = 'Todos os cursos';
+$string['all'] = 'Todas';
+$string['allpast'] = 'Todas as passadas';
+$string['attendance:canbelisted'] = 'Aparece na lista';
+$string['attendance:changeattendances'] = 'Alterando Presenças';
+$string['attendance:changepreferences'] = 'Alterando Preferências';
+$string['attendancedata'] = 'Dados de presença';
+$string['attendance:export'] = 'Exportar Relatórios';
+$string['attendanceforthecourse'] = 'Presença para o curso';
+$string['attendancegrade'] = 'Nota de presença';
+$string['attendance:manageattendances'] = 'Gerenciar Presenças';
+$string['attendancenotstarted'] = 'Presença ainda não foi iniciada para este curso';
+$string['attendancepercent'] = 'Percentual de presença';
+$string['attendancereport'] = 'Relatório de presença';
+$string['attendancesuccess'] = 'Presença registrada com sucesso';
+$string['attendance:takeattendances'] = 'Obtendo Presenças';
+$string['attendanceupdated'] = 'Presença atualizada com sucesso';
+$string['attendance:view'] = 'Visualizando Presenças';
+$string['attendance:viewreports'] = 'Visualizando Relatórios';
+$string['attforblockdirstillexists'] = 'O antigo diretório mod/attforblock ainda existe: você deve apagar esse diretório no seu servidor antes de realizar esta atualização.';
+$string['attrecords'] = 'Registros de presença';
+$string['calclose'] = 'Fechar';
+$string['calmonths'] = 'Janeiro,Fevereiro,Março,Abril,Maio,Junho,Julho,Agosto,Setembro,Outubro,Novembro,Dezembro';
+$string['calshow'] = 'Escolher data';
+$string['caltoday'] = 'Hoje';
+$string['calweekdays'] = 'Dom,Seg,Ter,Qua,Qui,Sex,Sáb';
+$string['cannottakeforgroup'] = 'Você não pode registrar a presença para o grupo "{$a}"';
+$string['changeattendance'] = 'Alterar presença';
+$string['changeduration'] = 'Alterar duração';
+$string['changesession'] = 'Alterar sessão';
+$string['column'] = 'coluna';
+$string['columns'] = 'colunas';
+$string['commonsession'] = 'Comum';
+$string['commonsessions'] = 'Comuns';
+$string['countofselected'] = 'Contagem da seleção';
+$string['copyfrom'] = 'Copiar dados de presença de';
+$string['createmultiplesessions'] = 'Criar várias sessões';
+$string['createmultiplesessions_help'] = 'Esta função possibilita a criação de várias sessões num único passo.
+
+ * <strong>Data de início da sessão</strong>: Selecione a data de início do curso (o primeiro dia de aulas)
+ * <strong>Data de término da sessão</strong>: Selecione o último dia de aulas (o último dia que você deseja registrar presenças).
+ * <strong>Dias da sessão</strong>: Selecione os dias da semana no qual haverá aula (por exemplo, Segunda/Quarta/Sexta).
+ * <strong>Frequência</strong>: Se há aulas todas as semanas, selecione 1; a cada duas semanas, selecione 2; e assim por diante.
+';
+$string['createonesession'] = 'Criar uma sessão para o curso';
+$string['days'] = 'Dia';
+$string['defaults'] = 'Padrões';
+$string['defaultdisplaymode'] = 'Modo de visualização padrão';
+$string['delete'] = 'Apagar';
+$string['deletelogs'] = 'Apagar registros de presença';
+$string['deleteselected'] = 'Apagar selecionado';
+$string['deletesession'] = 'Apagar sessão';
+$string['deletesessions'] = 'Apagar todas as sessões';
+$string['deletingsession'] = 'Apagando sessão para o curso';
+$string['deletingstatus'] = 'Apagando status para o curso';
+$string['description'] = 'Descrição';
+$string['display'] = 'Exibir';
+$string['displaymode'] = 'Modo de exibição';
+$string['downloadexcel'] = 'Download no formato Excel';
+$string['downloadooo'] = 'Download no formato OpenOffice';
+$string['downloadtext'] = 'Download no formato de texto';
+$string['donotusepaging'] = 'Não utilizar paginação';
+$string['duration'] = 'Duração';
+$string['editsession'] = 'Editar Sessão';
+$string['endtime'] = 'Horário final da sessão';
+$string['endofperiod'] = 'Término do período';
+$string['enrolmentend'] = 'Inscrição de usuários termina {$a}';
+$string['enrolmentstart'] = 'Inscrição de usuários inicia {$a}';
+$string['enrolmentsuspended'] = 'Inscrições suspensas';
+$string['errorgroupsnotselected'] = 'Selecione um ou mais grupos';
+$string['errorinaddingsession'] = 'Erro ao adicionar sessão';
+$string['erroringeneratingsessions'] = 'Erro ao gerar sessões';
+$string['gradebookexplanation'] = 'Nota no livro de notas';
+$string['gradebookexplanation_help'] = 'O módulo de Presença mostra sua nota de presença atual baseada no número de pontos que você ganhou e o número total de pontos que poderiam ter sido ganhos até agora; ele não inclui aulas futuras. No livro de notas, sua nota de presença é baseada na percentagem da sua presença atual e o número de pontos que poderiam ser ganhos ao longo de todo o período do curso, incluindo aulas futuras. Deste modo, suas notas de presença mostradas no módulo de Presença e no livro de notas podem não ser o mesmo número de pontos, mas eles são a mesma percentagem.
+
+Por exemplo, se você ganhou 8 de 10 pontos até agora (80% de presença) e a presença para todo o curso é de 50 pontos, o módulo de Presença mostrará 8/10 e o livro de notas mostrará 40/50. Você ainda não ganhou 40 pontos, mas 40 é o valor equivalente de pontos para a sua percentagem atual de presença de 80%. Os pontos que você ganhou no módulo de Presença nunca podem diminuir, pois ela é baseada apenas na presença registrada até o momento; no entanto, os pontos de presença mostrados no livro de notas podem aumentar ou diminuir baseada na sua futura frequência, pois ela é calculada tomando em conta toda a duração do curso.';
+$string['gridcolumns'] = 'Colunas da grade';
+$string['groupsession'] = 'Grupo';
+$string['hiddensessions'] = 'Sessões ocultas';
+$string['hiddensessions_help'] = 'Sessões deixam de ser visíveis se sua data é anterior à data de início do curso. Altere a data de início do curso e as sessões ocultas serão apresentadas.
+
+Você pode usar esta facilidade para ocultar sessões antigas em vez de excluí-las. Note que somente sessões visíveis são usadas no Livro de Notas.';
+$string['identifyby'] = 'Identificar estudante por';
+$string['includeall'] = 'Selecionar todas as sessões';
+$string['includenottaken'] = 'Incluir sessões não realizadas';
+$string['includeremarks'] = 'Incluir observações';
+$string['indetail'] = 'Em detalhe...';
+$string['invalidsessionenddate'] = 'A data final da sessão não pode ser anterior à data de início da sessão';
+$string['jumpto'] = 'Pular para';
+$string['maxgrade'] = 'Nota máxima';
+$string['maximumpoints'] = 'Pontos máximos';
+$string['maximumpoints_desc'] = 'Número padrão máximo para pontos quando o tipo de nota selecionado for pontos';
+$string['modulename'] = 'Presença';
+$string['modulename_help'] = 'O módulo de atividade de presença permite ao professor registrar a presença durante a aula e aos estudantes visualizarem os seus dados de frequência.
+
+O professor pode criar várias sessões e registrar a frequência como "Presente", "Ausente", "Atrasado" ou "Dispensado", ou modificar as opções de acordo com as suas necessidades.
+
+Relatórios estão disponíveis para toda a turma ou individualmente para cada aluno.';
+$string['modulenameplural'] = 'Presenças';
+$string['months'] = 'Meses';
+$string['moreattendance'] = 'A presença foi registrada com sucesso para esta página';
+$string['myvariables'] = 'Minhas Variáveis';
+$string['newdate'] = 'Nova data';
+$string['newduration'] = 'Nova duração';
+$string['noattforuser'] = 'Não existem registros de presença para o usuário';
+$string['nodescription'] = 'Sessão de aula regular';
+$string['noguest'] = 'Visitantes não podem ver presença';
+$string['nogroups'] = 'Você não pode adicionar sessões de grupo. Nenhum grupo existe no curso.';
+$string['noofdaysabsent'] = 'Nº de dias ausente';
+$string['noofdaysexcused'] = 'Nº de dias dispensado';
+$string['noofdayslate'] = 'Nª de dias atrasado';
+$string['noofdayspresent'] = 'Nº de dias presente';
+$string['nosessiondayselected'] = 'Nenhum dia de sessão selecionado';
+$string['nosessionexists'] = 'Nenhuma sessão existe para este curso';
+$string['nosessionsselected'] = 'Nenhuma sessão selecionada';
+$string['notfound'] = 'Atividade de presença não encontrada para o curso!';
+$string['noupgradefromthisversion'] = 'O módulo attendance não pode ser atualizado a partir da versão do attforblock instalado. Por favor, remova o attforblock ou atualize-o para a última versão antes de instalar o novo módulo attendance.';
+$string['numsessions'] = 'Número de sessões';
+$string['olddate'] = 'Antiga data';
+$string['pageof'] = 'Página {$a->page} de {$a->numberofpages}';
+$string['period'] = 'Frequência';
+$string['pluginadministration'] = 'Administração da presença';
+$string['pluginname'] = 'Presença';
+$string['remark'] = 'Observação para: {$a}';
+$string['remarks'] = 'Observações';
+$string['report'] = 'Relatório';
+$string['required'] = 'Obrigatório*';
+$string['resetdescription'] = 'Lembre-se que apagar os dados de presença irá apagar a informação do banco de dados. Você pode apenas esconder antigas sessões que tiveram a data de início do curso alteradas.';
+$string['resetstatuses'] = 'Redefinir status padrão';
+$string['restoredefaults'] = 'Restaurar padrões';
+$string['resultsperpage'] = 'Resultados por página';
+$string['resultsperpage_desc'] = 'Número de estudantes mostrados por página';
+$string['save'] = 'Salvar presença';
+$string['session'] = 'Sessão';
+$string['session_help'] = 'Sessão';
+$string['sessionadded'] = 'Sessão adicionada com sucesso';
+$string['sessionalreadyexists'] = 'Sessão já existe para esta data';
+$string['sessiondate'] = 'Data da sessão';
+$string['sessiondays'] = 'Dias da sessão';
+$string['sessiondeleted'] = 'Sessão apagada com sucesso';
+$string['sessionenddate'] = 'Data de término da sessão';
+$string['sessionexist'] = 'Sessão não adicionada (já existe)!';
+$string['sessions'] = 'Sessões';
+$string['sessionscompleted'] = 'Sessões registradas';
+$string['sessionsids'] = 'IDs das sessões';
+$string['sessionsgenerated'] = 'Sessões geradas com sucesso';
+$string['sessionsnotfound'] = 'Não existem sessões no intervalo de tempo selecionado';
+$string['sessionstartdate'] = 'Data de início da sessão';
+$string['sessiontype'] = 'Tipo de sessão';
+$string['sessiontype_help'] = 'Há dois tipos de sessões: regular e em grupos. A capacidade para definir tipos diferentes depende da modalidade de grupos da atividade:
+
+ * Na modalidade "Nenhum grupo" só é possível adicionar sessões regulares.
+ * Na modalidade "Grupos visíveis" é possível adicionar sessões regulares e sessões em grupo.
+ * Na modalidade  "Grupos separados" só é possível adicionar sessões em grupo.
+';
+$string['sessiontypeshort'] = 'Tipo';
+$string['sessionupdated'] = 'Sessão atualizada com sucesso';
+$string['setallstatusesto'] = 'Definir status de todos os usuário para «{$a}»';
+$string['settings'] = 'Configurações';
+$string['showdefaults'] = 'Mostrar padrões';
+$string['showduration'] = 'Mostrar duração';
+$string['sortedgrid'] = 'Grade ordenada';
+$string['sortedlist'] = 'Lista ordenada';
+$string['startofperiod'] = 'Início do período';
+$string['status'] = 'Status';
+$string['statuses'] = 'Status';
+$string['statusdeleted'] = 'Status apagado';
+$string['strftimedm'] = '%d/%m';
+$string['strftimedmy'] = '%d/%m/%Y';
+$string['strftimedmyhm'] = '%d/%m/%Y %H:%M';
+$string['strftimedmyw'] = '%d/%m/%y (%a)';
+$string['strftimehm'] = '%H:%M';
+$string['strftimeshortdate'] = '%d/%m/%Y';
+$string['studentid'] = 'ID do Estudante';
+$string['summary'] = 'Sumário';
+$string['takeattendance'] = 'Registrar presença';
+$string['thiscourse'] = 'Este curso';
+$string['tablerenamefailed'] = 'A renomeação da antiga tabela do attforblock para o attendance falhou';
+$string['update'] = 'Atualizar';
+$string['variable'] = 'variável';
+$string['variablesupdated'] = 'Variáveis atualizadas com sucesso';
+$string['versionforprinting'] = 'versão para impressão';
+$string['viewmode'] = 'Modo de visualização';
+$string['week'] = 'semana(s)';
+$string['weeks'] = 'Semanas';
+$string['youcantdo'] = 'Você não pode fazer nada';
+
+$string['eventreportviewed'] = 'Relatório de presença foi visualizado';
+$string['eventsessionadded'] = 'Sessão adicionada';
+$string['eventsessionupdated'] = 'Sessão atualizada';
+$string['eventtaken'] = 'Presença registrada';
+$string['eventtakenbystudent'] = 'Presença registrada por estudante';
+$string['eventsessiondeleted'] = 'Sessão removida';
+$string['eventdurationupdated'] = 'Duração da sessão atualizada';
+$string['eventstatusupdated'] = 'Status alterado';
+$string['eventstatusadded'] = 'Status adicionado';
+
+$string['studentscanmark'] = 'Permitir que os próprios estudantes registrem suas presenças';
+$string['studentscanmark_help'] = 'Se marcado, os próprios estudantes poderão alterar o status para a sessão.';
+$string['set_by_student'] = 'Autoregistrado';
+$string['attendance_already_submitted'] = 'Você não pode registrar presença que já foi previamente registrada.';
+$string['lowgrade'] = 'Notas baixas';
+$string['submitattendance'] = 'Registrar presença';
+$string['attendancenotset'] = 'Você precisa selecionar uma opção para a presença';
