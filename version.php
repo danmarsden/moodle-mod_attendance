ce7a9bc12ce4a34966211c5fdf0b216b3aa02492
Dan Marsden <dan@danmarsden.com>
2016-03-15 14:00:12 +1300

    Bump version in preparation for plugins db release

--- version.php
+++ version.php
@@ -22,9 +22,9 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version  = 2015102200;
+$plugin->version  = 2016031500;
 $plugin->requires = 2015051100;
-$plugin->release = '3.0.0.1';
+$plugin->release = '3.1.0.0';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->cron     = 0;
 $plugin->component = 'mod_attendance';
