69aab7e0dd2ad3ee206c47b36834d71ba6991f41
Daniel Thee Roperto <danielroperto@catalyst-au.net>
2017-03-07 18:03:40 +1100

    Fixes #216 and #243 - Adds the fields 'timemodified', 'intro' and 'introformat' in the attendance table.
    This is done according to https://docs.moodle.org/dev/Activity_modules#install.xml

--- db/upgrade.php
+++ db/upgrade.php
@@ -177,5 +177,34 @@ function xmldb_attendance_upgrade($oldversion=0) {
         upgrade_mod_savepoint(true, 2016052202, 'attendance');
     }
 
+    if ($oldversion < 2016052203) {
+
+        // Define field timemodified to be added to attendance.
+        $table = new xmldb_table('attendance');
+        $field = new xmldb_field('timemodified', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0', 'grade');
+
+        // Conditionally launch add field timemodified.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        // Define field timemodified to be added to attendance.
+        $table = new xmldb_table('attendance');
+
+        $fields = [];
+        $fields[] = new xmldb_field('intro', XMLDB_TYPE_TEXT, null, null, null, null, null, 'timemodified');
+        $fields[] = new xmldb_field('introformat', XMLDB_TYPE_INTEGER, '4', null, XMLDB_NOTNULL, null, 0, 'intro');
+
+        // Conditionally launch add field.
+        foreach ($fields as $field) {
+            if (!$dbman->field_exists($table, $field)) {
+                $dbman->add_field($table, $field);
+            }
+        }
+
+        // Attendance savepoint reached.
+        upgrade_mod_savepoint(true, 2016052203, 'attendance');
+    }
+
     return $result;
 }
