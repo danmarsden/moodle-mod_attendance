6fa00db94156916a7209caa65ffea5d8f31704cd
Daniel Thee Roperto <danielroperto@catalyst-au.net>
2017-03-07 18:03:40 +1100

    Fixes #216 and #243 - Adds the fields 'timemodified', 'intro' and 'introformat' in the attendance table.
    This is done according to https://docs.moodle.org/dev/Activity_modules#install.xml

--- version.php
+++ version.php
@@ -22,9 +22,9 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version  = 2016031501;
+$plugin->version  = 2016031502;
 $plugin->requires = 2015051100;
-$plugin->release = '3.1.0.1';
+$plugin->release = '3.1.0.2';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->cron     = 0;
 $plugin->component = 'mod_attendance';
