c2d04204d887ea204f724f3cc03a7483d734ebf6
Dan Marsden <dan@danmarsden.com>
2017-05-01 14:39:06 +1200

    Tidy up the classes/phpdocs a bit.

--- locallib.php
+++ locallib.php
@@ -400,3 +400,84 @@ function attendance_can_student_mark($sess) {
     }
     return $canmark;
 }
+
+/**
+ * Generate worksheet for Attendance export
+ *
+ * @param stdclass $data The data for the report
+ * @param string $filename The name of the file
+ * @param string $format excel|ods
+ *
+ */
+function attendance_exporttotableed($data, $filename, $format) {
+    global $CFG;
+
+    if ($format === 'excel') {
+        require_once("$CFG->libdir/excellib.class.php");
+        $filename .= ".xls";
+        $workbook = new MoodleExcelWorkbook("-");
+    } else {
+        require_once("$CFG->libdir/odslib.class.php");
+        $filename .= ".ods";
+        $workbook = new MoodleODSWorkbook("-");
+    }
+    // Sending HTTP headers.
+    $workbook->send($filename);
+    // Creating the first worksheet.
+    $myxls = $workbook->add_worksheet('Attendances');
+    // Format types.
+    $formatbc = $workbook->add_format();
+    $formatbc->set_bold(1);
+
+    $myxls->write(0, 0, get_string('course'), $formatbc);
+    $myxls->write(0, 1, $data->course);
+    $myxls->write(1, 0, get_string('group'), $formatbc);
+    $myxls->write(1, 1, $data->group);
+
+    $i = 3;
+    $j = 0;
+    foreach ($data->tabhead as $cell) {
+        // Merge cells if the heading would be empty (remarks column).
+        if (empty($cell)) {
+            $myxls->merge_cells($i, $j - 1, $i, $j);
+        } else {
+            $myxls->write($i, $j, $cell, $formatbc);
+        }
+        $j++;
+    }
+    $i++;
+    $j = 0;
+    foreach ($data->table as $row) {
+        foreach ($row as $cell) {
+            $myxls->write($i, $j++, $cell);
+        }
+        $i++;
+        $j = 0;
+    }
+    $workbook->close();
+}
+
+/**
+ * Generate csv for Attendance export
+ *
+ * @param stdclass $data The data for the report
+ * @param string $filename The name of the file
+ *
+ */
+function attendance_exporttocsv($data, $filename) {
+    $filename .= ".txt";
+
+    header("Content-Type: application/download\n");
+    header("Content-Disposition: attachment; filename=\"$filename\"");
+    header("Expires: 0");
+    header("Cache-Control: must-revalidate,post-check=0,pre-check=0");
+    header("Pragma: public");
+
+    echo get_string('course')."\t".$data->course."\n";
+    echo get_string('group')."\t".$data->group."\n\n";
+
+    echo implode("\t", $data->tabhead)."\n";
+    foreach ($data->table as $row) {
+        echo implode("\t", $row)."\n";
+    }
+}
