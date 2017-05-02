f2897f5d0b5cc7a78a65b340b399fbd35c5c589e
Dan Marsden <dan@danmarsden.com>
2017-05-02 12:42:08 +1200

    bump version for 3.3 plugins db release.

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2017050100;
+$plugin->version  = 2017050200;
 $plugin->requires = 2017011900;
 $plugin->release = '3.3.4';
 $plugin->maturity  = MATURITY_STABLE;
