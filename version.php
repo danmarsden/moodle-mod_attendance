be48371a17e7882fa40fb0aad989366dbfb9adac
Dan Marsden <dan@danmarsden.com>
2017-05-11 12:06:12 +1200

    bump version to clear css cache.

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2016121307;
+$plugin->version  = 2016121308;
 $plugin->requires = 2016111800;
 $plugin->release = '3.2.7';
 $plugin->maturity  = MATURITY_STABLE;
