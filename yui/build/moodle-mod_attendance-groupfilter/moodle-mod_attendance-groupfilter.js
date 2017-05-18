6445dbc3a1ecc055802b5ae06884ad63476f54f4
Dan Marsden <dan@danmarsden.com>
2017-05-18 12:55:26 +1200

    Fix some coding guideline issues.

--- yui/build/moodle-mod_attendance-groupfilter/moodle-mod_attendance-groupfilter.js
+++ yui/build/moodle-mod_attendance-groupfilter/moodle-mod_attendance-groupfilter.js
@@ -16,7 +16,7 @@ M.mod_attendance.groupfilter = {
     /**
      * Update the user list with those found in the selected group.
      */
-    update_user_list: function() {
+    update_user_list: function() { // eslint-disable-line camelcase
         "use strict";
         var groupid, userlist, users, userid, opt;
 
