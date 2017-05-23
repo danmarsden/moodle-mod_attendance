48df01e34f9a1827cab85ca131ed6689f0977c5b
Dan Marsden <dan@danmarsden.com>
2017-05-23 14:48:53 +1200

    New site-level/course category report for average course attendance.

--- version.php
+++ version.php
@@ -23,9 +23,9 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2017052201;
+$plugin->version  = 2017052300;
 $plugin->requires = 2017042100;
-$plugin->release = '3.3.5';
+$plugin->release = '3.3.6';
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->cron     = 0;
 $plugin->component = 'mod_attendance';
