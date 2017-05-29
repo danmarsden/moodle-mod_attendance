35c1712ed404923a8eabfd25f812dd7a1b63fa5f
Dan Marsden <dan@danmarsden.com>
2017-05-29 12:31:52 +1200

    bump version to match block requirements.

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2016121312;
+$plugin->version  = 2016121313;
 $plugin->requires = 2016111800;
 $plugin->release = '3.2.11';
 $plugin->maturity  = MATURITY_STABLE;
