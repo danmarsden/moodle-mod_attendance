d290ba044554615234bd22e12721c50267c44b3f
Dan Marsden <dan@danmarsden.com>
2017-07-13 13:11:05 +1200

    Allow warnings with same percentage but different warnlevel.

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2017062201;
+$plugin->version  = 2017071301;
 $plugin->requires = 2017042100;
 $plugin->release = '3.3.9';
 $plugin->maturity  = MATURITY_ALPHA;
