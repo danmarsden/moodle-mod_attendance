700c4b1bfbeade058193307695ad209ad8eed968
Dan Marsden <dan@danmarsden.com>
2017-07-13 13:11:05 +1200

    Allow warnings with same percentage but different warnlevel.

--- db/upgrade.php
+++ db/upgrade.php
@@ -351,7 +351,6 @@ function xmldb_attendance_upgrade($oldversion=0) {
 
         // Adding keys to table attendance_warning.
         $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
-        $table->add_key('level_id', XMLDB_KEY_UNIQUE, array('idnumber, warningpercent'));
 
         // Conditionally launch create table for attendance_warning.
         if (!$dbman->table_exists($table)) {
@@ -380,6 +379,19 @@ function xmldb_attendance_upgrade($oldversion=0) {
 
         // Attendance savepoint reached.
         upgrade_mod_savepoint(true, 2017050210, 'attendance');
+    }
+
+    if ($oldversion < 2017050212) {
+        // Fix key.
+        $table = new xmldb_table('attendance_warning');
+        if ($table->getkey('level_id')) {
+            $table->deleteKey('level_id');
+        }
+        $key = new xmldb_key('level_id', XMLDB_KEY_UNIQUE, array('idnumber, warningpercent, warnafter'));
+        $dbman->add_key($table, $key);
+
+        // Attendance savepoint reached.
+        upgrade_mod_savepoint(true, 2017050212, 'attendance');
     }
 
     return $result;
