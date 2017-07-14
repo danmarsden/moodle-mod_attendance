b9c07817f60b7c7a32a4b0e004da89c3d589fb27
Dan Marsden <dan@danmarsden.com>
2017-07-14 21:25:02 +1200

    Drop key doesn't work well, drop incorrect key and add correct one.

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2016121317;
+$plugin->version  = 2016121318;
 $plugin->requires = 2016111800;
 $plugin->release = '3.2.14';
 $plugin->maturity  = MATURITY_STABLE;
