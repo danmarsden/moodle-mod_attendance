89f5fcf0409ddb39753999946e3e294d98d5be11
Dan Marsden <dan@danmarsden.com>
2017-08-21 12:24:57 +1200

    Bump version.

--- version.php
+++ version.php
@@ -23,9 +23,9 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2017071802;
+$plugin->version  = 2017072100;
 $plugin->requires = 2017042100;
-$plugin->release = '3.3.10';
+$plugin->release = '3.3.11';
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->cron     = 0;
 $plugin->component = 'mod_attendance';
