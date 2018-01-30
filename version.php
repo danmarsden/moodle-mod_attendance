30c1814ff8c09ace9a18d557b0186905dd386944
Dan Marsden <dan@danmarsden.com>
2018-01-31 11:18:11 +1300

    Split out graded and ungraded sessions on students all courses list.

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2017120801;
+$plugin->version  = 2017120802;
 $plugin->requires = 2017102700; // Requires 3.4
 $plugin->release = '3.4.2';
 $plugin->maturity  = MATURITY_ALPHA;
