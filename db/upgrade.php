3f2adbc053b042805c9cf03d2975c1e6468e14ae
Nick Phillips <nick.phillips@otago.ac.nz>
2018-02-26 14:32:13 +1300

    Make display of user 'extrafields' optional in report. (#294)

--- db/upgrade.php
+++ db/upgrade.php
@@ -478,5 +478,15 @@ function xmldb_attendance_upgrade($oldversion=0) {
         upgrade_mod_savepoint(true, 2017120801, 'attendance');
     }
 
+    if ($oldversion < 2018022204) {
+        $table = new xmldb_table('attendance');
+        $field = new xmldb_field('showextrauserdetails', XMLDB_TYPE_INTEGER, '1', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '1', 'subnet');
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+        upgrade_mod_savepoint(true, 2018022204, 'attendance');
+    }
+
+    
     return $result;
 }
