0eaef1aa0ddbdf6857fc82838aa4458726c89972
Dan Marsden <dan@danmarsden.com>
2018-05-12 09:29:50 +1200

    master branch - now requires 3.5 or higher.

--- version.php
+++ version.php
@@ -23,9 +23,9 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2018050101;
-$plugin->requires = 2017102700; // Requires 3.4.
-$plugin->release = '3.4.4';
+$plugin->version  = 2018051400;
+$plugin->requires = 2018050800; // Requires 3.5.
+$plugin->release = '3.5.0';
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->cron     = 0;
 $plugin->component = 'mod_attendance';
