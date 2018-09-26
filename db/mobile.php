1d57b0ebfabfc7b46d0f22def21287eb6bdafdd8
Dan Marsden <dan@danmarsden.com>
2018-09-26 12:23:22 +1200

    Tidy up radio buttons used in teacher view.

--- db/mobile.php
+++ db/mobile.php
@@ -36,7 +36,7 @@ $addons = [
                 'method' => 'mobile_view_activity',
                 'styles' => [
                     'url' => '/mod/attendance/mobilestyles.css',
-                    'version' => 7
+                    'version' => 19
                 ]
             ]
         ],
