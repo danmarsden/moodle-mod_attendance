4f8410a5a074cdcc19b924beffacb27a94aaa3f7
Dan Marsden <dan@danmarsden.com>
2018-12-04 09:13:36 +1300

    fix maturity of branch.

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version  = 2018102600;
 $plugin->requires = 2018102700; // Requires 3.6.
 $plugin->release = '3.6.3';
-$plugin->maturity  = MATURITY_ALPHA;
+$plugin->maturity  = MATURITY_STABLE;
 $plugin->cron     = 0;
 $plugin->component = 'mod_attendance';
