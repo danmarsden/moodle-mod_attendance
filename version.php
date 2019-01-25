7154e7c79ee661b7d8b8618b743fe6f7094c0916
Dan Marsden <dan@danmarsden.com>
2019-01-25 19:56:16 +1300

    Fix #359 - increase number of allowed status items in the logs. This is still a bit fragile, but with over 10 times the existing number it should be enough for now.

--- version.php
+++ version.php
@@ -23,9 +23,9 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2018102600;
+$plugin->version  = 2019012500;
 $plugin->requires = 2018102700; // Requires 3.6.
-$plugin->release = '3.6.3';
+$plugin->release = '3.6.4';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->cron     = 0;
 $plugin->component = 'mod_attendance';
