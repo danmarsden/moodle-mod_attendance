8d486b6b54a8f949d97864585ea92277bcd0261d
Dan Marsden <dan@danmarsden.com>
2019-01-29 15:25:13 +1300

    Fix #340 Check duration of session when checking if a student enrolled before a session.

--- renderer.php
+++ renderer.php
@@ -1101,7 +1101,7 @@ class mod_attendance_renderer extends plugin_renderer_base {
                 $row->cells[] = format_float($status->grade, 1, true, true) . ' / ' .
                                     format_float($statussetmaxpoints[$status->setnumber], 1, true, true);
                 $row->cells[] = $sess->remarks;
-            } else if ($sess->sessdate < $userdata->user->enrolmentstart) {
+            } else if (($sess->sessdate + $sess->duration) < $userdata->user->enrolmentstart) {
                 $cell = new html_table_cell(get_string('enrolmentstart', 'attendance',
                                             userdate($userdata->user->enrolmentstart, '%d.%m.%Y')));
                 $cell->colspan = 3;
