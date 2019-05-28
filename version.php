e235ae8588a49224696f0e6270f26503ac1230ff
Dan Marsden <dan@danmarsden.com>
2019-05-28 13:00:29 +1200

    Bump version for plugins db release.

--- version.php
+++ version.php
@@ -23,9 +23,9 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2019042600;
+$plugin->version  = 2019052800;
 $plugin->requires = 2018102700; // Requires 3.6.
-$plugin->release = '3.6.6';
+$plugin->release = '3.6.7';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->cron     = 0;
 $plugin->component = 'mod_attendance';
