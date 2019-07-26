b9d7d159cd6c1aba5057af553bd4351541095bee
Dan Marsden <dan@danmarsden.com>
2019-07-26 13:30:21 +1200

    Set require Moodle 3.7 and bump version higher than 3.6 branch.

--- version.php
+++ version.php
@@ -23,9 +23,9 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2019072100;
-$plugin->requires = 2018102700; // Requires 3.6.
-$plugin->release = '3.6.7';
+$plugin->version  = 2019072400;
+$plugin->requires = 2019052000; // Requires 3.7.
+$plugin->release = '3.7.0';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->cron     = 0;
 $plugin->component = 'mod_attendance';
