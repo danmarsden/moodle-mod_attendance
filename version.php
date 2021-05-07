ba9d39bcc34ba947b55f139d85b96ce4320f7cee
Dan Marsden <dan@danmarsden.com>
2021-05-07 23:08:55 +1200

    3.11 branch

--- version.php
+++ version.php
@@ -23,9 +23,9 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2021050700;
-$plugin->requires = 2019072500; // Requires 3.8.
-$plugin->release = '3.9.1';
+$plugin->version  = 2021060700;
+$plugin->requires = 2021050400; // Requires 3.11.
+$plugin->release = '3.11.1';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->cron     = 0;
 $plugin->component = 'mod_attendance';
