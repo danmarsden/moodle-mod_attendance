b673ba878ec454136a0e12cf8d8d27f60e6a49c8
Dan Marsden <dan@danmarsden.com>
2021-07-02 19:45:36 +1200

    Bump attendance version.

--- version.php
+++ version.php
@@ -23,9 +23,9 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2021062901;
+$plugin->version  = 2021070200;
 $plugin->requires = 2021050400; // Requires 3.11.
-$plugin->release = '3.11.3';
+$plugin->release = '3.11.4';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->cron     = 0;
 $plugin->component = 'mod_attendance';
