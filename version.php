256835431724171a0eb41b930104ee7881f367cf
Dan Marsden <dan@danmarsden.com>
2021-08-17 15:14:30 +1200

    bump version to test plugins db release.

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2021072304;
+$plugin->version  = 2021072305;
 $plugin->requires = 2021050400; // Requires 3.11.
 $plugin->release = '3.11.5';
 $plugin->maturity  = MATURITY_STABLE;
