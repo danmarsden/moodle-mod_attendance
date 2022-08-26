354cf755dfd7b19b0c473a3da7aa43858da2d866
Dan Marsden <dan@danmarsden.com>
2022-08-26 13:59:52 +1200

    Bump version for release with new reportbuilder sources.

--- version.php
+++ version.php
@@ -23,9 +23,9 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2022082300;
+$plugin->version  = 2022082600;
 $plugin->requires = 2022031100; // Requires 4.0.
-$plugin->release = '4.0.4';
+$plugin->release = '4.0.5';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->cron     = 0;
 $plugin->component = 'mod_attendance';
