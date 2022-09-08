12abd0b1fe207f289357d2c6c7119ca758cf70dc
Dan Marsden <dan@danmarsden.com>
2022-09-09 09:54:39 +1200

    Clean up duplicate records if record has the same sessionid, studentid, statusid only retain one record.

--- version.php
+++ version.php
@@ -23,9 +23,9 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2022090600;
+$plugin->version  = 2022090900;
 $plugin->requires = 2022090200; // Requires 4.0.
-$plugin->release = '2022090600';
+$plugin->release = '2022090900';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->cron     = 0;
 $plugin->component = 'mod_attendance';
