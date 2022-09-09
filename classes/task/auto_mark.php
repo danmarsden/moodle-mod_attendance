8150b176d1734b9dc57b8a0ecf8c42bc173f8654
Dan Marsden <dan@danmarsden.com>
2022-09-09 12:08:40 +1200

    Restructure auto-grading function to allow re-use.

--- classes/task/auto_mark.php
+++ classes/task/auto_mark.php
@@ -50,234 +50,34 @@ class auto_mark extends \core\task\scheduled_task {
      * Execte the task.
      */
     public function execute() {
-        global $DB, $CFG;
+        global $DB;
         // Create some cache vars - might be nice to restructure this and make a smaller number of sql calls.
         $cachecm = array();
         $cacheatt = array();
         $cachecourse = array();
-        $now = time(); // Store current time to use in queries so they all match nicely.
         $sql = "SELECT se.*, ss.id as setunmarked
                   FROM {attendance_sessions} se
              LEFT JOIN {attendance_statuses} ss ON ss.attendanceid = se.attendanceid
                        AND ss.setunmarked = 1 AND ss.deleted = 0 AND ss.setnumber = se.statusset
              WHERE se.automark > 0 AND se.automarkcompleted < 2 AND se.sessdate < ?";
 
-        $sessions = $DB->get_recordset_sql($sql, [$now]);
+        $sessions = $DB->get_recordset_sql($sql, [time()]);
 
         foreach ($sessions as $session) {
-            if ($session->sessdate + $session->duration < $now || // If session is over.
-                // OR if session is currently open and automark is set to do all.
-                ($session->sessdate < $now && $session->automark == 1)) {
-
-                $userfirstaccess = array();
-                $donesomething = false; // Only trigger grades/events when an update actually occurs.
-                $sessionover = false; // Is this session over?
-                if ($session->sessdate + $session->duration < $now) {
-                    $sessionover = true;
-                }
-
-                // Store cm/att/course in cachefields so we don't make unnecessary db calls.
-
-                if (empty($session->setunmarked)) {
-                    $coursemodule = get_coursemodule_from_instance('attendance', $session->attendanceid);
-                    $url = $CFG->wwwroot.'/mod/attendance/preferences.php?id='.$coursemodule->id;
-                    mtrace("No unmarked status configured for session id: ".$session->id. " to fix, go to: ".$url);
-                    continue;
-                }
-
-                if (empty($cacheatt[$session->attendanceid])) {
-                    $cacheatt[$session->attendanceid] = $DB->get_record('attendance', array('id' => $session->attendanceid));
-                }
-                if (empty($cachecm[$session->attendanceid])) {
-                    $cachecm[$session->attendanceid] = get_coursemodule_from_instance('attendance',
-                        $session->attendanceid, $cacheatt[$session->attendanceid]->course);
-                }
-                $courseid = $cacheatt[$session->attendanceid]->course;
-                if (empty($cachecourse[$courseid])) {
-                    $cachecourse[$courseid] = $DB->get_record('course', array('id' => $courseid));
-                }
-                $context = \context_module::instance($cachecm[$session->attendanceid]->id);
-
-                $pageparams = new \mod_attendance_take_page_params();
-                $pageparams->group = $session->groupid;
-                if (empty($session->groupid)) {
-                    $pageparams->grouptype  = 0;
-                } else {
-                    $pageparams->grouptype  = 1;
-                }
-                $pageparams->sessionid  = $session->id;
-
-                $att = new \mod_attendance_structure($cacheatt[$session->attendanceid],
-                $cachecm[$session->attendanceid], $cachecourse[$courseid], $context, $pageparams);
-
-                if ($session->automark == 1) {
-                    $userfirstacess = array();
-                    // If set to do full automarking, get all users that have accessed course during session open.
-                    $id = $DB->sql_concat('userid', 'ip'); // Users may access from multiple ip, make the first field unique.
-                    $sql = "SELECT $id, userid, ip, min(timecreated) as timecreated
-                             FROM {logstore_standard_log}
-                            WHERE courseid = ? AND timecreated > ? AND timecreated < ?
-                         GROUP BY userid, ip";
-
-                    $timestart = $session->sessdate;
-                    if (empty($session->lasttakenby) && $session->lasttaken > $timestart) {
-                        // If the last time session was taken it was done automatically, use the last time taken
-                        // as the start time for the logs we are interested in to help with performance.
-                        $timestart = $session->lasttaken;
-                    }
-                    $duration = $session->duration;
-                    if (empty($duration)) {
-                        $duration = get_config('attendance', 'studentscanmarksessiontimeend') * 60;
-                    }
-                    $timeend = $timestart + $duration;
-                    $logusers = $DB->get_recordset_sql($sql, array($courseid, $timestart, $timeend));
-                    // Check if user access is in allowed subnet.
-                    foreach ($logusers as $loguser) {
-                        if (!empty($session->subnet) && !address_in_subnet($loguser->ip, $session->subnet)) {
-                            // This record isn't in the right subnet.
-                            continue;
-                        }
-                        if (empty($userfirstaccess[$loguser->userid]) ||
-                            $userfirstaccess[$loguser->userid] > $loguser->timecreated) {
-                            // Users may have accessed from mulitple ip addresses, find the earliest access.
-                            $userfirstaccess[$loguser->userid] = $loguser->timecreated;
-                        }
-                    }
-                    $logusers->close();
-
-                } else if ($session->automark == 3) {
-                    $existinglog = $DB->get_records_menu('attendance_log',
-                        ['sessionid' => $session->id], '', 'studentid, statusid');
-
-                    $newlogs = [];
-
-                    // Get users who have completed the course in this session.
-                    $completedusers = $DB->get_records_select('course_modules_completion',
-                        'coursemoduleid = ? AND completionstate > 0', [$session->automarkcmid]);
-
-                    // Get automark status the users and update the attendance log.
-                    foreach ($completedusers as $completionuser) {
-                        if (empty($completionuser->timemodified) || (empty($completionuser->userid))) {
-                            // Time modified or userid not set - we can't calculate for this record.
-                            continue;
-                        }
-                        if (!empty($existinglog[$completionuser->userid])) {
-                            // Status already set for this user.
-                            continue;
-                        }
-                        if (!has_capability('mod/attendance:canbelisted', $context, $completionuser->userid)) {
-                            // This user can't be listed in this attendance - skip them.
-                            continue;
-                        }
-                        if (!empty($session->groupid) && !groups_is_member($session->groupid, $completionuser->userid)) {
-                            // This is a group session, and the user is not a member of the group.
-                            continue;
-                        }
-                        $newlog = new \stdClass();
-                        $newlog->timetaken = $now;
-                        $newlog->takenby = 0;
-                        $newlog->sessionid = $session->id;
-                        $newlog->remarks = get_string('autorecorded', 'attendance');
-                        $newlog->statusset = implode(',', array_keys( (array)$att->get_statuses()));
-                        $newlog->studentid = $completionuser->userid;
-                        $newlog->statusid = $att->get_automark_status($completionuser->timemodified, $session->id);
-                        if (!empty($newlog->statusid)) {
-                            $newlogs[] = $newlog;
-                        }
-                    }
-                    if (!empty($newlogs)) {
-                        $DB->insert_records('attendance_log', $newlogs);
-                    }
-                }
-
-                // Get all unmarked students.
-                $users = $att->get_users($session->groupid, 0);
-
-                $existinglog = $DB->get_recordset('attendance_log', array('sessionid' => $session->id));
-                $updated = 0;
-
-                foreach ($existinglog as $log) {
-                    if (empty($log->statusid)) {
-                        if ($sessionover || !empty($userfirstaccess[$log->studentid])) {
-                            // Status needs updating.
-                            if (!empty($userfirstaccess[$log->studentid])) {
-                                $log->statusid = $att->get_automark_status($userfirstaccess[$log->studentid], $session->id);
-                            } else if ($sessionover) {
-                                $log->statusid = $session->setunmarked;
-                            }
-                            if (!empty($log->statusid)) {
-                                $log->timetaken = $now;
-                                $log->takenby = 0;
-                                $log->remarks = get_string('autorecorded', 'attendance');
-
-                                $DB->update_record('attendance_log', $log);
-                                $updated++;
-                                $donesomething = true;
-                            }
-                        }
-                    }
-                    unset($users[$log->studentid]);
-                }
-                $existinglog->close();
-                mtrace($updated . " session status updated");
-
-                $newlogs = [];
-
-                $added = 0;
-                foreach ($users as $user) {
-                    if ($sessionover || !empty($userfirstaccess[$user->id])) {
-                        $newlog = new \stdClass();
-                        $newlog->timetaken = $now;
-                        $newlog->takenby = 0;
-                        $newlog->sessionid = $session->id;
-                        $newlog->remarks = get_string('autorecorded', 'attendance');
-                        $newlog->statusset = implode(',', array_keys( (array)$att->get_statuses()));
-                        if (!empty($userfirstaccess[$user->id])) {
-                            $newlog->statusid = $att->get_automark_status($userfirstaccess[$user->id], $session->id);
-                        } else if ($sessionover) {
-                            $newlog->statusid = $session->setunmarked;
-                        }
-                        if (!empty($newlog->statusid)) {
-                            $newlog->studentid = $user->id;
-                            $newlogs[] = $newlog;
-                            $added++;
-                        }
-                    }
-                }
-                if (!empty($newlogs)) {
-                    $DB->insert_records('attendance_log', $newlogs);
-                    $donesomething = true;
-                }
-                mtrace($added . " session status inserted");
-
-                // Update lasttaken time and automarkcompleted for this session.
-                $session->lasttaken = $now;
-                $session->lasttakenby = 0;
-                if ($sessionover) {
-                    $session->automarkcompleted = 2;
-                } else {
-                    $session->automarkcompleted = 1;
-                }
-
-                $DB->update_record('attendance_sessions', $session);
-
-                if ($donesomething) {
-                    if ($att->grade != 0) {
-                        $att->update_users_grade(array_keys($users));
-                    }
-
-                    $params = array(
-                        'sessionid' => $att->pageparams->sessionid,
-                        'grouptype' => $att->pageparams->grouptype);
-                    $event = \mod_attendance\event\attendance_taken::create(array(
-                        'objectid' => $att->id,
-                        'context' => $att->context,
-                        'other' => $params));
-                    $event->add_record_snapshot('course_modules', $att->cm);
-                    $event->add_record_snapshot('attendance_sessions', $session);
-                    $event->trigger();
-                }
+            if (empty($cacheatt[$session->attendanceid])) {
+                $cacheatt[$session->attendanceid] = $DB->get_record('attendance', array('id' => $session->attendanceid));
             }
+            if (empty($cachecm[$session->attendanceid])) {
+                $cachecm[$session->attendanceid] = get_coursemodule_from_instance('attendance',
+                    $session->attendanceid, $cacheatt[$session->attendanceid]->course);
+            }
+            $courseid = $cacheatt[$session->attendanceid]->course;
+            if (empty($cachecourse[$courseid])) {
+                $cachecourse[$courseid] = $DB->get_record('course', array('id' => $courseid));
+            }
+            \mod_attendance\local\automark::session($session, $cachecourse[$courseid], $cachecm[$session->attendanceid],
+                                                    $cacheatt[$session->attendanceid]);
+
         }
     }
 }
