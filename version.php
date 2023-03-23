47ee04925646440757586f103016faec9aa01963
Dan Marsden <dan@danmarsden.com>
2023-03-23 20:35:34 +1300

    bump version for release.

--- version.php
+++ version.php
@@ -23,10 +23,10 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2023020104;
-$plugin->release = '2023020104';
-$plugin->requires = 2022090200; // Requires 4.1.
+$plugin->version   = 2023020105;
+$plugin->release   = 2023020105;
+$plugin->requires  = 2022090200; // Requires 4.1.
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->cron     = 0;
+$plugin->cron      = 0;
 $plugin->component = 'mod_attendance';
 $plugin->supported = [401, 401];
