8425f949516cfbf8ab3f887cbf0f690e7141ab33
chiou <chiou@posboss.com.tw>
2025-04-26 21:52:03 +0800

    version changed

--- version.php
+++ version.php
@@ -23,8 +23,8 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2024072401;
-$plugin->release = 2024072401;
+$plugin->version  = 2025042601;
+$plugin->release = 2025042601;
 $plugin->requires = 2024092700; // Requires 4.5.
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->cron     = 0;
