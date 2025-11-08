e54c96a3949f84b4f83be1b5c44a4fb430e8f1da
Zoltán Sasvári <zoltan@sasvari.org>
2025-11-08 09:31:46 +0100

    Strip tags from remarks content on display.

--- classes/output/renderer.php
+++ classes/output/renderer.php
@@ -1435,7 +1435,7 @@ class renderer extends plugin_renderer_base {
                 $row->cells[] = $status->description . $updatelink;
                 $row->cells[] = format_float($status->grade, 1, true, true) . ' / ' .
                                     format_float($statussetmaxpoints[$status->setnumber], 1, true, true);
-                $row->cells[] = $sess->remarks;
+                $row->cells[] = s($sess->remarks);
             } else if (($sess->sessdate + $sess->duration) < $userdata->user->enrolmentstart) {
                 $cell = new html_table_cell(get_string(
                     'enrolmentstart',
@@ -2078,14 +2078,14 @@ class renderer extends plugin_renderer_base {
                         $cell = new html_table_cell($celltext);
                         $row->cells[] = $cell;
 
-                        $celltext = empty($ucdata['remarks']) ? '' : $ucdata['remarks'];
+                        $celltext = empty($ucdata['remarks']) ? '' : s($ucdata['remarks']);
                         $cell = new html_table_cell($celltext);
                         $row->cells[] = $cell;
                     } else {
                         if (!empty($sess->statusid)) {
                             $status = $userdata->statuses[$sess->attendanceid][$sess->statusid];
                             $row->cells[] = $status->description;
-                            $row->cells[] = $sess->remarks;
+                            $row->cells[] = s($sess->remarks);
                         }
                     }
                 } else {
@@ -2094,7 +2094,7 @@ class renderer extends plugin_renderer_base {
                         $row->cells[] = $status->description;
                         $row->cells[] = format_float($status->grade, 1, true, true) . ' / ' .
                             format_float($statussetmaxpoints[$status->setnumber], 1, true, true);
-                        $row->cells[] = $sess->remarks;
+                        $row->cells[] = s($sess->remarks);
                     } else if (($sess->sessdate + $sess->duration) < $userdata->user->enrolmentstart) {
                         $cell = new html_table_cell(get_string(
                             'enrolmentstart',
