61712573bcf4dca26ec9e976df884fdbffdb1edc
Dan Marsden <dan@danmarsden.com>
2025-11-11 11:02:34 +1300

    bump version higher to make sure different from 4.4 branch.

--- version.php
+++ version.php
@@ -23,8 +23,8 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2024072402;
-$plugin->release = 2024072402;
+$plugin->version  = 2024082400;
+$plugin->release = 2024082400;
 $plugin->requires = 2024092700; // Requires 4.5.
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->cron     = 0;
