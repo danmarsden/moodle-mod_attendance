ed9b2a8911ff4841f6c952d04f866f86f554e147
Luca Bösch <luca.boesch@bfh.ch>
2026-03-25 17:25:15 +0100

    Comply to codechecker.

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -37,9 +37,9 @@ use stdClass;
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 final class provider implements
+    \core_privacy\local\metadata\provider,
     \core_privacy\local\request\core_userlist_provider,
-    \core_privacy\local\request\plugin\provider,
-    \core_privacy\local\metadata\provider {
+    \core_privacy\local\request\plugin\provider {
     /**
      * Returns meta data about this system.
      *
