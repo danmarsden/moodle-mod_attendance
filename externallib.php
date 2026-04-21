7ab926358b65b7d0441c5b6832e6b8ad8ac6e40b
Levi Grenz <levigrenz@web.de>
2026-04-21 13:26:29 +0200

    Fix access control in update_user_status

--- externallib.php
+++ externallib.php
@@ -549,6 +549,11 @@ class mod_attendance_external extends external_api {
 
         // If not a teacher, make sure session is open for self-marking.
         if (!has_capability('mod/attendance:takeattendances', $context)) {
+            global $USER;
+            if ($params['studentid'] != $USER->id || $params['takenbyid'] != $USER->id) {
+                throw new invalid_parameter_exception('Invalid user id or no permissions.');
+            }
+
             [$canmark, $reason] = attendance_can_student_mark($session);
             if (!$canmark) {
                 throw new invalid_parameter_exception($reason);
