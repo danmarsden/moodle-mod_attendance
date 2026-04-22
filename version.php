1abc3281937df1c3f90dafc110eb76228b746549
Dan Marsden <dan@danmarsden.com>
2026-04-22 15:25:46 +1200

    bump version for release.

--- version.php
+++ version.php
@@ -23,10 +23,10 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2025122100;
-$plugin->release = 2025111100;
+$plugin->version  = 2025122101;
+$plugin->release = 2025122101;
 $plugin->requires = 2025031100; // Requires 5.0.
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->cron     = 0;
 $plugin->component = 'mod_attendance';
-$plugin->supported = [500, 501];
+$plugin->supported = [500, 500];
