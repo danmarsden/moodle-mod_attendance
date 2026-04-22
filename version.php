be917d4dfcd91cea72631ac8052321ca3f85115c
Dan Marsden <dan@danmarsden.com>
2026-04-22 15:29:58 +1200

    Update plugin version and release numbers

--- version.php
+++ version.php
@@ -23,8 +23,8 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2024082401;
-$plugin->release = 2024082401;
+$plugin->version  = 2024082402;
+$plugin->release = 2024082402;
 $plugin->requires = 2024092700; // Requires 4.5.
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->cron     = 0;
